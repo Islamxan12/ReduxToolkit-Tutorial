e254b58b06606ddaaa4e99c512ffe30ccf31c0ee
Islamxan <islamkhandeveloper@gmail.com>
2023-01-04 14:44:14 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route} from 'react-router-dom'
-import { Main, Login, Register } from './components';
+import { Main, Login, Register, Navbar } from './components';
 function App() {
  
 
   return (
     <>
+    <Navbar/>
     <Routes>
     <Route path='/'  element={<Main/>}/>
      <Route path='/login'  element={<Login/>}/>
